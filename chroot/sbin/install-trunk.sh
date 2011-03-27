10016248c5282ef97d0eade1e3fc30f44a3dcaf5
Darshan Shaligram <scintilla@gmail.com>
2011-03-27 09:19:09 +0530

    Add copyright notices.

--- chroot/sbin/install-trunk.sh
+++ chroot/sbin/install-trunk.sh
@@ -3,6 +3,25 @@
 # This script needs a sudo entry for user crawl to run as root:
 # For obvious reasons, this script should not source any other scripts that
 # are not owned by root.
+#
+# ===========================================================================
+# Copyright (C) 2008, 2009, 2010, 2011 Marc H. Thoben
+# Copyright (C) 2011 Darshan Shaligram
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# ===========================================================================
+#
 
 set -e
 set -u
