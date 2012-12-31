e2b35adf631103ba99434430e776f92b02fc8da5
Dobrazupa.org staff <crawl-dev@dobrazupa.org>
2012-12-30 22:34:44 -0500

    Remove inception launcher.

--- config.py
+++ config.py
@@ -155,16 +155,6 @@ games = OrderedDict([
         ttyrec_path = "%%CHROOT_TTYRECDIR%%/%n/",
         socket_path = "%%CHROOT_WEBDIR%%/sockets",
         options = ["-tutorial"])),
-
-     ("dcss-inception", dict(
-        name = "Inception",
-        crawl_binary = "/usr/games/crawl-inception",
-        rcfile_path = "%%CHROOT_RCFILESDIR%%/crawl-git/",
-        macro_path = "%%CHROOT_RCFILESDIR%%/crawl-git/",
-        morgue_path = "%%CHROOT_MORGUEDIR%%/%n/",
-        inprogress_path = "%%CHROOT_INPROGRESSDIR%%/crawl-inception/",
-        ttyrec_path = "%%CHROOT_TTYRECDIR%%/%n/",
-        socket_path = "%%CHROOT_WEBDIR%%/sockets")),
 ])
 
 dgl_status_file = "%%CHROOT_WEBDIR%%/run/status"
