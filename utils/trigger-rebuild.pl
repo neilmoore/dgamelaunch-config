5e2431b76b64e86ad5472045ab87db64518a5d8e
Dobrazupa.org staff <crawl-dev@dobrazupa.org>
2013-12-30 11:57:54 -0500

    Add vine stalker experimental branch.

--- utils/trigger-rebuild.pl
+++ utils/trigger-rebuild.pl
@@ -151,10 +151,10 @@ sub main() {
   my $ver = param('v');
 
   if (not $ver) {
-    do_prompt 'trunk', '0.13', '0.12', '0.11', '0.10';
+    do_prompt 'trunk', '0.13', '0.12', '0.11', '0.10', 'vinestalker';
   } elsif ($ver eq 'trunk') {
     do_update 'trunk';
-  } elsif ($ver =~ /^0.1[0123]$/) {
+  } elsif ($ver =~ /^0.1[0123]|vinestalker$/) {
     do_update 'stable', $ver;
   } else {
     do_fail "Unknown version " . escapeHTML($ver);
