caa76ceba594355671c98a39d532b59a52863651
John Boyle <boylejm@gmail.com>
2014-01-03 20:10:06 -0500

    initial updates to get server up and running

--- config.py
+++ config.py
@@ -7,13 +7,14 @@ except ImportError:
 dgl_mode = True
 
 bind_nonsecure = True # Set to false to only use SSL
-bind_address = "66.111.46.147"
-bind_port = 80
+bind_address = "192.73.239.18"
 
-bind_pairs = (
-    ("209.222.93.171", 80),
-    ("66.111.46.147", 80),
-)
+bind_port = 8080
+
+#bind_pairs = (
+#    ("192.73.239.18", 8080),
+#    ("192.73.239.18", 8080),
+#)
 
 logging_config = {
     "filename": "%%CHROOT_WEBDIR%%/run/webtiles.log",
@@ -30,7 +31,7 @@ template_path = "%%CHROOT_WEBDIR%%/templates/"
 server_socket_path = None # Uses global temp dir
 
 # Server name, so far only used in the ttyrec metadata
-server_id = "cszo"
+server_id = "cbn"
 
 # Disable caching of game data files
 game_data_no_cache = False
@@ -125,137 +126,6 @@ games = OrderedDict([
         socket_path = "%%CHROOT_WEBDIR%%/sockets",
         options = ["-tutorial"])),
     
-    ("dcss-0.12", dict(
-        name = "DCSS 0.12",
-        crawl_binary = "/bin/crawl-stable-launcher.sh",
-        pre_options  = [ "0.12" ],
-        rcfile_path = "%%CHROOT_RCFILESDIR%%/crawl-0.12/",
-        macro_path = "%%CHROOT_RCFILESDIR%%/crawl-0.12/",
-        morgue_path = "%%CHROOT_MORGUEDIR%%/%n/",
-        inprogress_path = "%%CHROOT_INPROGRESSDIR%%/crawl-12/",
-        ttyrec_path = "%%CHROOT_TTYRECDIR%%/%n/",
-        socket_path = "%%CHROOT_WEBDIR%%/sockets")),
-    ("spr-0.12", dict(
-        name = "Sprint 0.12",
-        crawl_binary = "/bin/crawl-stable-launcher.sh",
-        pre_options  = [ "0.12" ],
-        rcfile_path = "%%CHROOT_RCFILESDIR%%/crawl-0.12/",
-        macro_path = "%%CHROOT_RCFILESDIR%%/crawl-0.12/",
-        morgue_path = "%%CHROOT_MORGUEDIR%%/%n/",
-        inprogress_path = "%%CHROOT_INPROGRESSDIR%%/crawl-12-sprint/",
-        ttyrec_path = "%%CHROOT_TTYRECDIR%%/%n/",
-        socket_path = "%%CHROOT_WEBDIR%%/sockets",
-        options = ["-sprint"])),
-    ("zd-0.12", dict(
-        name = "Zot Defence 0.12",
-        crawl_binary = "/bin/crawl-stable-launcher.sh",
-        pre_options  = [ "0.12" ],
-        rcfile_path = "%%CHROOT_RCFILESDIR%%/crawl-0.12/",
-        macro_path = "%%CHROOT_RCFILESDIR%%/crawl-0.12/",
-        morgue_path = "%%CHROOT_MORGUEDIR%%/%n/",
-        inprogress_path = "%%CHROOT_INPROGRESSDIR%%/crawl-12-zotdef/",
-        ttyrec_path = "%%CHROOT_TTYRECDIR%%/%n/",
-        socket_path = "%%CHROOT_WEBDIR%%/sockets",
-        options = ["-zotdef"])),
-    ("tut-0.12", dict(
-        name = "Tutorial 0.12",
-        crawl_binary = "/bin/crawl-stable-launcher.sh",
-        pre_options  = [ "0.12" ],
-        rcfile_path = "%%CHROOT_RCFILESDIR%%/crawl-0.12/",
-        macro_path = "%%CHROOT_RCFILESDIR%%/crawl-0.12/",
-        morgue_path = "%%CHROOT_MORGUEDIR%%/%n/",
-        inprogress_path = "%%CHROOT_INPROGRESSDIR%%/crawl-12-tut/",
-        ttyrec_path = "%%CHROOT_TTYRECDIR%%/%n/",
-        socket_path = "%%CHROOT_WEBDIR%%/sockets",
-        options = ["-tutorial"])),
-
-    ("dcss-0.11", dict(
-        name = "DCSS 0.11",
-        crawl_binary = "/bin/crawl-stable-launcher.sh",
-        pre_options  = [ "0.11" ],
-        rcfile_path = "%%CHROOT_RCFILESDIR%%/crawl-0.11/",
-        macro_path = "%%CHROOT_RCFILESDIR%%/crawl-0.11/",
-        morgue_path = "%%CHROOT_MORGUEDIR%%/%n/",
-        inprogress_path = "%%CHROOT_INPROGRESSDIR%%/crawl-11/",
-        ttyrec_path = "%%CHROOT_TTYRECDIR%%/%n/",
-        socket_path = "%%CHROOT_WEBDIR%%/sockets")),
-    ("spr-0.11", dict(
-        name = "Sprint 0.11",
-        crawl_binary = "/bin/crawl-stable-launcher.sh",
-        pre_options  = [ "0.11" ],
-        rcfile_path = "%%CHROOT_RCFILESDIR%%/crawl-0.11/",
-        macro_path = "%%CHROOT_RCFILESDIR%%/crawl-0.11/",
-        morgue_path = "%%CHROOT_MORGUEDIR%%/%n/",
-        inprogress_path = "%%CHROOT_INPROGRESSDIR%%/crawl-11-sprint/",
-        ttyrec_path = "%%CHROOT_TTYRECDIR%%/%n/",
-        socket_path = "%%CHROOT_WEBDIR%%/sockets",
-        options = ["-sprint"])),
-    ("zd-0.11", dict(
-        name = "Zot Defence 0.11",
-        crawl_binary = "/bin/crawl-stable-launcher.sh",
-        pre_options  = [ "0.11" ],
-        rcfile_path = "%%CHROOT_RCFILESDIR%%/crawl-0.11/",
-        macro_path = "%%CHROOT_RCFILESDIR%%/crawl-0.11/",
-        morgue_path = "%%CHROOT_MORGUEDIR%%/%n/",
-        inprogress_path = "%%CHROOT_INPROGRESSDIR%%/crawl-11-zotdef/",
-        ttyrec_path = "%%CHROOT_TTYRECDIR%%/%n/",
-        socket_path = "%%CHROOT_WEBDIR%%/sockets",
-        options = ["-zotdef"])),
-    ("tut-0.11", dict(
-        name = "Tutorial 0.11",
-        crawl_binary = "/bin/crawl-stable-launcher.sh",
-        pre_options  = [ "0.11" ],
-        rcfile_path = "%%CHROOT_RCFILESDIR%%/crawl-0.11/",
-        macro_path = "%%CHROOT_RCFILESDIR%%/crawl-0.11/",
-        morgue_path = "%%CHROOT_MORGUEDIR%%/%n/",
-        inprogress_path = "%%CHROOT_INPROGRESSDIR%%/crawl-11-tut/",
-        ttyrec_path = "%%CHROOT_TTYRECDIR%%/%n/",
-        socket_path = "%%CHROOT_WEBDIR%%/sockets",
-        options = ["-tutorial"])),
-
-    ("dcss-0.10", dict(
-        name = "DCSS 0.10",
-        crawl_binary = "/bin/crawl-stable-launcher.sh",
-        pre_options  = [ "0.10" ],
-        rcfile_path = "%%CHROOT_RCFILESDIR%%/crawl-0.10/",
-        macro_path = "%%CHROOT_RCFILESDIR%%/crawl-0.10/",
-        morgue_path = "%%CHROOT_MORGUEDIR%%/%n/",
-        inprogress_path = "%%CHROOT_INPROGRESSDIR%%/crawl-10/",
-        ttyrec_path = "%%CHROOT_TTYRECDIR%%/%n/",
-        socket_path = "%%CHROOT_WEBDIR%%/sockets")),
-    ("spr-0.10", dict(
-        name = "Sprint 0.10",
-        crawl_binary = "/bin/crawl-stable-launcher.sh",
-        pre_options  = [ "0.10" ],
-        rcfile_path = "%%CHROOT_RCFILESDIR%%/crawl-0.10/",
-        macro_path = "%%CHROOT_RCFILESDIR%%/crawl-0.10/",
-        morgue_path = "%%CHROOT_MORGUEDIR%%/%n/",
-        inprogress_path = "%%CHROOT_INPROGRESSDIR%%/crawl-10-sprint/",
-        ttyrec_path = "%%CHROOT_TTYRECDIR%%/%n/",
-        socket_path = "%%CHROOT_WEBDIR%%/sockets",
-        options = ["-sprint"])),
-    ("zd-0.10", dict(
-        name = "Zot Defence 0.10",
-        crawl_binary = "/bin/crawl-stable-launcher.sh",
-        pre_options  = [ "0.10" ],
-        rcfile_path = "%%CHROOT_RCFILESDIR%%/crawl-0.10/",
-        macro_path = "%%CHROOT_RCFILESDIR%%/crawl-0.10/",
-        morgue_path = "%%CHROOT_MORGUEDIR%%/%n/",
-        inprogress_path = "%%CHROOT_INPROGRESSDIR%%/crawl-10-zotdef/",
-        ttyrec_path = "%%CHROOT_TTYRECDIR%%/%n/",
-        socket_path = "%%CHROOT_WEBDIR%%/sockets",
-        options = ["-zotdef"])),
-    ("tut-0.10", dict(
-        name = "Tutorial 0.10",
-        crawl_binary = "/bin/crawl-stable-launcher.sh",
-        pre_options  = [ "0.10" ],
-        rcfile_path = "%%CHROOT_RCFILESDIR%%/crawl-0.10/",
-        macro_path = "%%CHROOT_RCFILESDIR%%/crawl-0.10/",
-        morgue_path = "%%CHROOT_MORGUEDIR%%/%n/",
-        inprogress_path = "%%CHROOT_INPROGRESSDIR%%/crawl-10-tut/",
-        ttyrec_path = "%%CHROOT_TTYRECDIR%%/%n/",
-        socket_path = "%%CHROOT_WEBDIR%%/sockets",
-        options = ["-tutorial"])),
 ])
 
 dgl_status_file = "%%CHROOT_WEBDIR%%/run/status"
@@ -274,19 +144,19 @@ max_connections = 200
 # at the moment.
 init_player_program = "/bin/init-webtiles.sh"
 
-#ssl_options = None # No SSL
-ssl_options = {
-    "certfile": "/etc/ssl/private/s-z.org.crt",
-    "keyfile": "/etc/ssl/private/s-z.org.key",
-    "ca_certs": "/etc/ssl/private/cas.pem"
-}
-ssl_address = "66.111.46.147"
-ssl_port = 443
+ssl_options = None # No SSL
+#ssl_options = {
+#    "certfile": "/etc/ssl/private/s-z.org.crt",
+#    "keyfile": "/etc/ssl/private/s-z.org.key",
+#    "ca_certs": "/etc/ssl/private/cas.pem"
+#}
+#ssl_address = "192.73.239.18"
+#ssl_port = 443
 
-ssl_bind_pairs = (
-    ("209.222.93.171", 443),
-    ("66.111.46.147", 443),
-)
+#ssl_bind_pairs = (
+#    ("192.73.239.18", 443),
+#    ("192.73.239.18", 443),
+#)
 
 connection_timeout = 600
 max_idle_time = 5 * 60 * 60
@@ -310,8 +180,8 @@ crypt_salt_length = 16
 
 login_token_lifetime = 7 # Days
 
-uid = 1002  # If this is not None, the server will setuid to that (numeric) id
-gid = 1002  # after binding its sockets.
+uid = 1007  # If this is not None, the server will setuid to that (numeric) id
+gid = 1008  # after binding its sockets.
 
 umask = None # e.g. 0077
 
