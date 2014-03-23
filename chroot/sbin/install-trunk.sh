f11c286de341e3518ffa2662351798c0d49a7168
Dobrazupa.org staff <crawl-dev@dobrazupa.org>
2014-03-23 00:46:37 -0400

    Install README.txt alongside docs (rchandra)

--- chroot/sbin/install-trunk.sh
+++ chroot/sbin/install-trunk.sh
@@ -59,7 +59,7 @@ copy-game-binary() {
 
 copy-data-files() {
     echo "Copying game data files to $DATADIR"
-    cp -r source/dat docs settings $DATADIR
+    cp -r source/dat README.txt docs settings $DATADIR
     cp -r settings/. $DGL_SETTINGS_DIR/$GAME-settings
     cp -r source/webserver/game_data/. $DATADIR/web
     cp -r source/webserver/!(config.py|game_data|templates) $WEBDIR
