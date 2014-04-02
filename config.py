9104f921a5a6c9d546e6cfe09d394d7b4b4e16a2
Dobrazupa.org staff <crawl-dev@dobrazupa.org>
2014-04-02 13:17:07 -0400

    Add nostalgia as an experimental branch.

--- config.py
+++ config.py
@@ -353,6 +353,19 @@ games = OrderedDict([
         inprogress_path = "%%CHROOT_INPROGRESSDIR%%/crawl-smithgod",
         ttyrec_path = "%%CHROOT_TTYRECDIR%%/%n/",
         socket_path = "%%CHROOT_WEBDIR%%/sockets")),
+
+    ("dcss-nostalgia", dict(
+        name = "Nostalgia",
+        crawl_binary = "/bin/crawl-stable-launcher.sh",
+        pre_options  = [ "nostalgia" ],
+        separator = "|",
+        rcfile_path = "%%CHROOT_RCFILESDIR%%/crawl-git/",
+        macro_path = "%%CHROOT_RCFILESDIR%%/crawl-git/",
+        morgue_path = "%%CHROOT_MORGUEDIR%%/%n/",
+        morgue_url = "http://dobrazupa.org/morgue/%n/",
+        inprogress_path = "%%CHROOT_INPROGRESSDIR%%/crawl-nostalgia",
+        ttyrec_path = "%%CHROOT_TTYRECDIR%%/%n/",
+        socket_path = "%%CHROOT_WEBDIR%%/sockets")),
 ])
 
 dgl_status_file = "%%CHROOT_WEBDIR%%/run/status"
