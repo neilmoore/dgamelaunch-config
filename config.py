2fdedb197882a86d8f91ef055f1e54e359093742
John Boyle <boylejm@gmail.com>
2014-04-01 23:00:20 -0400

    added experimental nostalgia branch

--- config.py
+++ config.py
@@ -175,7 +175,16 @@ games = OrderedDict([
         socket_path = "%%CHROOT_WEBDIR%%/sockets",
         options = ["-tutorial"])),
 
-    
+     ("dcss-nostalgia", dict(
+        name = "experimental (nostalgia)",
+        crawl_binary = "/usr/games/crawl-nostalgia",
+        rcfile_path = "%%CHROOT_RCFILESDIR%%/crawl-git/",
+        macro_path = "%%CHROOT_RCFILESDIR%%/crawl-git/",
+        morgue_path = "%%CHROOT_MORGUEDIR%%/%n/",
+        inprogress_path = "%%CHROOT_INPROGRESSDIR%%/crawl-nostalgia",
+        ttyrec_path = "%%CHROOT_TTYRECDIR%%/%n/",
+        socket_path = "%%CHROOT_WEBDIR%%/sockets")),    
+
 ])
 
 dgl_status_file = "%%CHROOT_WEBDIR%%/run/status"
