274ae8bf12a4a3e385664394a84f427f4b899bd5
John Boyle <boylejm@gmail.com>
2014-07-10 19:42:00 -0400

    Revert "Revert "trying to add update mechanism""
    This reverts commit dde5cd6ef27ba0f78d5c63284b690e191649f03a.

--- chroot/bin/dwarf-fortress-launcher.sh
+++ chroot/bin/dwarf-fortress-launcher.sh
@@ -21,33 +21,40 @@ dflauncher="/bin/df-launch.sh"
 dwizzell="/bin/dwizzell.pl.sh"
 MAXGAMES=6
 inprogressdir="/dgldir/inprogress/dwarf-fortress"
+userdirbak="/dfdir/${major_version}/bak_df_$user"
 
 #check to see if dir exist
+#if so, updateflag = 1, indicating we need to update, not new install
+update=1
 
 if [ ! -d "$userdir" ]; then
-
   mkdir "$userdir"
+  update=0
+else
+  cp -R $userdir $userdirbak
+  update=1
+fi
 
-  cd "$userdir"
-  (cd "$dfdir"; find -type d ! -name .) |xargs mkdir -p
+cd "$userdir"
+(cd "$dfdir"; find -type d ! -name .) |xargs mkdir -p
 
-  cd "$userdir"
-  for file in `find $dfdir -type f`
-  do
-      dfdir_path=$(dirname $file)
-      user_path="${dfdir_path/$dfdir/$userdir}"
-      cd "$user_path"
-      ln  $dfdir_path/$(basename $file)
-  done
+cd "$userdir"
+for file in `find $dfdir -type f`
+do
+    dfdir_path=$(dirname $file)
+    user_path="${dfdir_path/$dfdir/$userdir}"
+    cd "$user_path"
+    ln  $dfdir_path/$(basename $file)
+done
 
-  chmod -R 755 "$user_path"
+chmod -R 755 "$user_path"
 
+#then delete saved games
+rm -r "$userdir/data/save"
+mkdir "$userdir/data/save"
 
-  #then delete saved games
-  rm -r "$userdir/data/save"
-  mkdir "$userdir/data/save"
-
-  #then add default world
+if [ "${update}" = "0" ]; then
+  #only copy default world if not update
   cp -R $dfdir/data/save/region1 $userdir/data/save/
 
   #delete the gamelog.txt then touch the new one
@@ -58,16 +65,17 @@ if [ ! -d "$userdir" ]; then
   rm "$userdir/data/init/*"
   cp "$dfdir/data/init/*" "$userdir/data/init/"
 
-
-   
+else
+  cp -R $userdirbak/data/save $userdir/data/save/
+  cp -R $userdirbak/gamelog.txt $userdir/gamelog.txt
+  cp $userdirbak/data/init/* $userdir/data/init/
 fi
 
 cp "$dflauncher" "$userdir/df-launch.sh"
 
 #use sed to replace XXXXXX with $user in dwizzell.pl and save it there.
-sed "s/df_XXXXXX/\/${major_version}\/${user}/g" $dwizzell > "$userdir/dwizzell.pl"
-
-
+sed "s/|df_XXXXXX/|df_${user}/g" $dwizzell |\
+sed "s/df_XXXXXX/${major_version}\/df_${user}/g" > "$userdir/dwizzell.pl" 
 
 #check to see if we have enough open slots
 num_current_games=`ls -1 ${inprogressdir} |wc -l`
