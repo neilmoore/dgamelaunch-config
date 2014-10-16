db1ecb224064bb8f229c801533046e6c70309719
John Boyle <boylejm@gmail.com>
2014-10-16 05:14:08 -0400

    figured out client_path for new toml webtiles

--- crawl-build/update-crawl-stable-build.sh
+++ crawl-build/update-crawl-stable-build.sh
@@ -36,9 +36,9 @@ VER_STR="$(git-do describe HEAD)"
 VER_STR_OLD="$(($CRAWL_BINARY_PATH/$GAME -version 2>/dev/null || true) | sed -ne 's/Crawl version //p')"
 REVISION_OLD="${VER_STR_OLD##*-g}"
 
-
-[[ "$REVISION" == "$REVISION_OLD" || "$VER_STR" == "$VER_STR_OLD" ]] && \
-    abort-saying "Nothing new to install at the moment: you asked for $REVISION_FULL and it's already installed"
+#remove since we sometimes want to rebuild 
+#[[ "$REVISION" == "$REVISION_OLD" || "$VER_STR" == "$VER_STR_OLD" ]] && \
+#    abort-saying "Nothing new to install at the moment: you asked for $REVISION_FULL and it's already installed"
 
 prompt "start update build"
 
