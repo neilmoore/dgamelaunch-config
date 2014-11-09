06bd6f2132a7add69d488c283353c976ae430edb
John Boyle <boylejm@gmail.com>
2014-11-09 12:55:21 -0500

    s/dominoes/domino/g

--- crawl-build/update-crawl-stable-build.sh
+++ crawl-build/update-crawl-stable-build.sh
@@ -3,7 +3,7 @@ VERSION=${1:-0.11}
 
 # Quoting for =~ changed from bash 3.0 to 3.2; using a variable for the
 # regexp works with both.
-VERS_RE='^[0-9]+.[0-9]+|helen|farmer|potion_fun|webtiles-changes|bloaxcrawl|vaulted|dominoes|megavamps$'
+VERS_RE='^[0-9]+.[0-9]+|helen|farmer|potion_fun|webtiles-changes|bloaxcrawl|vaulted|domino|megavamps$'
 if [[ ! $VERSION =~ $VERS_RE ]]; then
     echo "Bad crawl version $VERSION"
     exit 1
