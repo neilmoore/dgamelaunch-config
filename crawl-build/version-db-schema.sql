cf88c92dd74ad4d32850c3d12afbd8783808c093
Dobrazupa.org staff <crawl-dev@dobrazupa.org>
2014-12-07 15:23:30 -0500

    Create a db of stable/experimental branches on first build.

--- crawl-build/version-db-schema.sql
+++ crawl-build/version-db-schema.sql
@@ -4,3 +4,10 @@ CREATE TABLE versions(hash TEXT(7) PRIMARY KEY,
                       major INTEGER,
                       minor INTEGER,
                       wizard INTEGER);
+CREATE TABLE branches(name TEXT(50) NOT NULL PRIMARY KEY,
+                      git TEXT(50) NOT NULL,
+                      description STRING NOT NULL,
+                      abbrev TEXT(1) NOT NULL,
+                      sprintabbr TEXT(1), -- null means no sprint
+                      ord INTEGER,        -- null to disable?
+                      wizard INTEGER NOT NULL); -- boolean?
