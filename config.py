e4648275030acf9b6bca99f0e04cfe1164e8c642
John Boyle <boylejm@gmail.com>
2014-12-26 01:10:36 -0500

    add evoker-god experimental

--- config.py
+++ config.py
@@ -273,6 +273,17 @@ games = OrderedDict([
         inprogress_path = "%%CHROOT_INPROGRESSDIR%%/crawl-no_backtracking_god",
         ttyrec_path = "%%CHROOT_TTYRECDIR%%/%n/",
         socket_path = "%%CHROOT_WEBDIR%%/sockets")),
+
+     ("dcss-evoker-god", dict(
+        name = "experimental (Pakellas, Evoker God)",
+        crawl_binary = "/usr/games/crawl-evoker-god",
+        separator = "<br>",
+        rcfile_path = "%%CHROOT_RCFILESDIR%%/crawl-git/",
+        macro_path = "%%CHROOT_RCFILESDIR%%/crawl-git/",
+        morgue_path = "%%CHROOT_MORGUEDIR%%/%n/",
+        inprogress_path = "%%CHROOT_INPROGRESSDIR%%/crawl-evoker-god",
+        ttyrec_path = "%%CHROOT_TTYRECDIR%%/%n/",
+        socket_path = "%%CHROOT_WEBDIR%%/sockets")),
 ])
 
 dgl_status_file = "%%CHROOT_WEBDIR%%/run/status"
