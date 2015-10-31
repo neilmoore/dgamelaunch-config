25cbe06d45592362c0fd31bc8fab7192c4fe74c9
Dobrazupa.org staff <crawl-dev@dobrazupa.org>
2015-10-31 13:53:35 -0400

    Add 0.17, make it the default.
    Conflicts:
    	chroot/data/menus/main_admin.txt
    	chroot/data/menus/main_user.txt
    	config.py
    	dgamelaunch.conf

--- config.py
+++ config.py
@@ -36,6 +36,57 @@ watch_socket_dirs = True
 # Game configs
 # %n in paths is replaced by the current username
 games = OrderedDict([
+    ("dcss-0.17", dict(
+        name = "DCSS 0.17",
+        crawl_binary = "/bin/crawl-stable-launcher.sh",
+        send_json_options = True,
+        pre_options = [ "0.17" ],
+        rcfile_path = "%%CHROOT_RCFILESDIR%%/crawl-0.17/",
+        macro_path = "%%CHROOT_RCFILESDIR%%/crawl-0.17/",
+        morgue_path = "%%CHROOT_MORGUEDIR%%/%n/",
+        morgue_url = "http://crawl.akrasiac.org/rawdata/%n/",
+        inprogress_path = "%%CHROOT_INPROGRESSDIR%%-crawl17/",
+        ttyrec_path = "%%CHROOT_TTYRECDIR%%/%n/",
+        socket_path = "%%CHROOT_WEBDIR%%/sockets")),
+    ("spr-0.17", dict(
+        name = "Sprint 0.17",
+        crawl_binary = "/bin/crawl-stable-launcher.sh",
+        send_json_options = True,
+        pre_options = [ "0.17" ],
+        rcfile_path = "%%CHROOT_RCFILESDIR%%/crawl-0.17/",
+        macro_path = "%%CHROOT_RCFILESDIR%%/crawl-0.17/",
+        morgue_path = "%%CHROOT_MORGUEDIR%%/%n/",
+        morgue_url = "http://crawl.akrasiac.org/rawdata/%n/",
+        inprogress_path = "%%CHROOT_INPROGRESSDIR%%-crawl17-sprint/",
+        ttyrec_path = "%%CHROOT_TTYRECDIR%%/%n/",
+        socket_path = "%%CHROOT_WEBDIR%%/sockets",
+        options = ["-sprint"])),
+    
+
+    ("dcss-git", dict(
+        name = "DCSS trunk",
+        crawl_binary = "/bin/crawl-git-launcher.sh",
+        send_json_options = True,
+        rcfile_path = "%%CHROOT_RCFILESDIR%%/crawl-git/",
+        macro_path = "%%CHROOT_RCFILESDIR%%/crawl-git/",
+        morgue_path = "%%CHROOT_MORGUEDIR%%/%n/",
+        morgue_url = "http://crawl.akrasiac.org/rawdata/%n/",
+        inprogress_path = "%%CHROOT_INPROGRESSDIR%%-crawl-git/",
+        ttyrec_path = "%%CHROOT_TTYRECDIR%%/%n/",
+        socket_path = "%%CHROOT_WEBDIR%%/sockets")),
+    ("spr-git", dict(
+        name = "Sprint trunk",
+        crawl_binary = "/bin/crawl-git-launcher.sh",
+        send_json_options = True,
+        rcfile_path = "%%CHROOT_RCFILESDIR%%/crawl-git/",
+        macro_path = "%%CHROOT_RCFILESDIR%%/crawl-git/",
+        morgue_path = "%%CHROOT_MORGUEDIR%%/%n/",
+        morgue_url = "http://crawl.akrasiac.org/rawdata/%n/",
+        inprogress_path = "%%CHROOT_INPROGRESSDIR%%-crawl-git-sprint/",
+        ttyrec_path = "%%CHROOT_TTYRECDIR%%/%n/",
+        socket_path = "%%CHROOT_WEBDIR%%/sockets",
+        options = ["-sprint"])),
+    
     ("dcss-0.16", dict(
         name = "DCSS 0.16",
         crawl_binary = "/bin/crawl-stable-launcher.sh",
@@ -62,30 +113,6 @@ games = OrderedDict([
         socket_path = "%%CHROOT_WEBDIR%%/sockets",
         options = ["-sprint"])),
 
-    ("dcss-git", dict(
-        name = "DCSS trunk",
-        crawl_binary = "/bin/crawl-git-launcher.sh",
-        send_json_options = True,
-        rcfile_path = "%%CHROOT_RCFILESDIR%%/crawl-git/",
-        macro_path = "%%CHROOT_RCFILESDIR%%/crawl-git/",
-        morgue_path = "%%CHROOT_MORGUEDIR%%/%n/",
-        morgue_url = "http://crawl.akrasiac.org/rawdata/%n/",
-        inprogress_path = "%%CHROOT_INPROGRESSDIR%%-crawl-git/",
-        ttyrec_path = "%%CHROOT_TTYRECDIR%%/%n/",
-        socket_path = "%%CHROOT_WEBDIR%%/sockets")),
-    ("spr-git", dict(
-        name = "Sprint trunk",
-        crawl_binary = "/bin/crawl-git-launcher.sh",
-        send_json_options = True,
-        rcfile_path = "%%CHROOT_RCFILESDIR%%/crawl-git/",
-        macro_path = "%%CHROOT_RCFILESDIR%%/crawl-git/",
-        morgue_path = "%%CHROOT_MORGUEDIR%%/%n/",
-        morgue_url = "http://crawl.akrasiac.org/rawdata/%n/",
-        inprogress_path = "%%CHROOT_INPROGRESSDIR%%-crawl-git-sprint/",
-        ttyrec_path = "%%CHROOT_TTYRECDIR%%/%n/",
-        socket_path = "%%CHROOT_WEBDIR%%/sockets",
-        options = ["-sprint"])),
-    
     ("dcss-0.15", dict(
         name = "DCSS 0.15",
         crawl_binary = "/bin/crawl-stable-launcher.sh",
