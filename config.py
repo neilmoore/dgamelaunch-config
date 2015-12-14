bd4961ad10b23723e0956daa3c99e9752e54880f
John Boyle <boylejm@gmail.com>
2015-12-13 22:04:22 -0500

    add back lacertilians

--- config.py
+++ config.py
@@ -298,6 +298,17 @@ games = OrderedDict([
         ttyrec_path = "%%CHROOT_TTYRECDIR%%/%n/",
         socket_path = "%%CHROOT_WEBDIR%%/sockets")),
 
+    ("dcss-faithful", dict(
+        name = "Lacertilians! (newly updated for Trunk!)",
+        crawl_binary = "/bin/crawl-stable-launcher.sh",
+        separator = "<br>",
+	pre_options  = [ "faithful" ],
+        rcfile_path = "%%CHROOT_RCFILESDIR%%/crawl-git/",
+        macro_path = "%%CHROOT_RCFILESDIR%%/crawl-git/",
+        morgue_path = "%%CHROOT_MORGUEDIR%%/%n/",
+        inprogress_path = "%%CHROOT_INPROGRESSDIR%%/crawl-faithful/",
+        ttyrec_path = "%%CHROOT_TTYRECDIR%%/%n/",
+        socket_path = "%%CHROOT_WEBDIR%%/sockets")),
 ])
 
 dgl_status_file = "%%CHROOT_WEBDIR%%/run/status"
@@ -325,6 +336,7 @@ milestone_file = [
  "%%CHROOT_CRAWL_BASEDIR%%/crawl-abyssrun/saves/milestones",
  "%%CHROOT_CRAWL_BASEDIR%%/crawl-evoker-god-rebase/saves/milestones",
  "%%CHROOT_CRAWL_BASEDIR%%/crawl-orcs_and_elves/saves/milestones",
+ "%%CHROOT_CRAWL_BASEDIR%%/crawl-faithful/saves/milestones",
  "%%CHROOT_CRAWL_GAMEDIR%%/saves/milestones",
  "%%CHROOT_CRAWL_GAMEDIR%%/saves/milestones-tutorial",
  "%%CHROOT_CRAWL_GAMEDIR%%/saves/milestones-sprint"
