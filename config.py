2a140174b9c2cc6a28de1d93ada237b9b5660fc5
John Boyle <boylejm@gmail.com>
2018-06-13 17:31:19 -0400

    add goon crawl

--- config.py
+++ config.py
@@ -462,6 +462,18 @@ games = OrderedDict([
         inprogress_path = "%%CHROOT_INPROGRESSDIR%%/crawl-hellcrawl-cbro/",
         ttyrec_path = "%%CHROOT_TTYRECDIR%%/%n/",
 	socket_path = "%%CHROOT_WEBDIR%%/sockets")),
+   
+   ("dcss-gooncrawl-cbro", dict(
+        name = "GoonCrawl! (Crawl, but designed by committee)",
+        crawl_binary = "/bin/crawl-stable-launcher.sh",
+  	separator = "<br>",
+        pre_options  = [ "gooncrawl-cbro" ],
+        rcfile_path = "%%CHROOT_RCFILESDIR%%/crawl-git/",
+        macro_path = "%%CHROOT_RCFILESDIR%%/crawl-git/",
+        morgue_path = "%%CHROOT_MORGUEDIR%%/%n/",
+        inprogress_path = "%%CHROOT_INPROGRESSDIR%%/crawl-gooncrawl-cbro/",
+        ttyrec_path = "%%CHROOT_TTYRECDIR%%/%n/",
+	socket_path = "%%CHROOT_WEBDIR%%/sockets")),
 
 ])
 
