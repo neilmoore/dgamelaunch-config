ee0a8b0433748031af0eeddab87e01901ef16d61
crawl.akrasiac.org staff <crawl-dev@crawl.akrasiac.org>
2018-08-06 22:12:00 -0700

    finish adding 0.21

--- utils/trigger-rebuild.pl
+++ utils/trigger-rebuild.pl
@@ -151,10 +151,10 @@ sub main() {
   my $ver = param('v');
 
   if (not $ver) {
-    do_prompt 'trunk', '0.20', '0.19', '0.18', '0.17', '0.16', '0.15', '0.14', '0.13', '0.12', '0.11', '0.10';
+    do_prompt 'trunk', '0.21', '0.20', '0.19', '0.18', '0.17', '0.16', '0.15', '0.14', '0.13', '0.12', '0.11', '0.10';
   } elsif ($ver eq 'trunk') {
     do_update 'trunk';
-  } elsif ($ver =~ /^0.1[0123456789]$|^0.2[0]$/) {
+  } elsif ($ver =~ /^0.1[0123456789]$|^0.2[01]$/) {
     do_update 'stable', $ver;
   } else {
     do_fail "Unknown version " . escapeHTML($ver);
